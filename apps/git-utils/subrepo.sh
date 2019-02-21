02eb7f72d236050e8f0173a664086eb54c9083bf
Yehonal <yehonal.azeroth@gmail.com>
2019-02-21 13:30:11 +0100

    minor fix

--- apps/git-utils/subrepo.sh
+++ apps/git-utils/subrepo.sh
@@ -1,8 +1,12 @@
 
-source "../git-subrepo/.rc"
+#!/usr/bin/env bash
+
+CUR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+
+source "$CUR_PATH/../git-subrepo/.rc"
 
 echo "> Init and updating submodules..."
-[ ! -d "apps/git-subrepo" ] && git submodule add https://github.com/ingydotnet/git-subrepo apps/git-subrepo
+[ ! -d "$CUR_PATH/../git-subrepo" ] && git submodule add https://github.com/ingydotnet/git-subrepo apps/git-subrepo
 git submodule update --init apps/git-subrepo
 
 function subrepoUpdate() {
