e984b869b0dd5ced7acf711a87552d5e568d3ea4
Yehonal <yehonal.azeroth@gmail.com>
2019-02-21 13:34:43 +0100

    node-platform dep

--- app.sh
+++ app.sh
@@ -7,4 +7,7 @@ ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 source "$ROOT_PATH/apps/git-utils/subrepo.sh"
 
 echo "> Pulling and update all subrepos"
+
 subrepoUpdate https://github.com/yehonal/sequelize-graphql-schema develop deps/sequelize-graphql-schema/
+
+subrepoUpdate https://gitlab.com/hw-core/node-platform master deps/hw-core/node-platform
