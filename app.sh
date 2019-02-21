ac62a9b5a6ebab57b93c2ff09a36642a969240a5
Yehonal <yehonal.azeroth@gmail.com>
2019-02-21 13:30:53 +0100

    minor fix 2

--- app.sh
+++ app.sh
@@ -7,4 +7,4 @@ ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 source "$ROOT_PATH/apps/git-utils/subrepo.sh"
 
 echo "> Pulling and update all subrepos"
-subrepoUpdate https://github.com/yehonal/sequelize-graphql-schema develop clean deps/sequelize-graphql-schema/
+subrepoUpdate https://github.com/yehonal/sequelize-graphql-schema develop deps/sequelize-graphql-schema/
