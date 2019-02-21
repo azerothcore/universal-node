15666c308d3ed9c7f65a8fb0c2e9a537b6466ab9
Yehonal <yehonal.azeroth@gmail.com>
2019-02-21 13:23:42 +0100

    squash to avoid issues with new repo

--- app.sh
+++ app.sh
@@ -13,5 +13,5 @@ source "$ROOT_PATH/apps/git-subrepo/.rc"
 echo "> Pulling and update all subrepos"
 git subrepo clean deps/sequelize-graphql-schema/
 git subrepo pull deps/sequelize-graphql-schema/
-git subrepo push deps/sequelize-graphql-schema/
+git subrepo push deps/sequelize-graphql-schema/ -s
 git subrepo clean deps/sequelize-graphql-schema/
